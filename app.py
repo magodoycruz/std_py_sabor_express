2ff0aa3f127078a19d2621157220d647d475b3a1
Marcela Godoy <magodoycruz@gmail.com>
2025-03-05 07:40:18 -0300

    Manipulação de Strings e arquivo de playground para testes

---
+++ app.py
@@ -0,0 +1,13 @@
+# Frescurinha de FSymbol para título bonitinho
+print('''
+█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+''')
+
+print('1. Cadastrar Restaurante')
+print('2. Listar Restaurantes')
+print('3. Ativar Restaurante')
+print('4. Sair \n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}')
---
+++ playground.py
@@ -0,0 +1,12 @@
+print('Python na Escola de Programação Alura \n')
+
+nome = 'Marcela'
+idade = 36
+
+print(f'Meu nome é {nome} e eu tenho {idade} anos.\n')
+
+print('a\nl\nu\nr\na\n')
+
+pi = 3.14159
+pi_arredondado = round(pi, 2) # Arredonda, exibindo 2 casas decimais (segundo parametro)
+print(f'O valor arredondado de pi é {pi_arredondado}')
