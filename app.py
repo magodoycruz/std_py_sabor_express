c6118e4b27cbba3cd79b559c3b766f6fde3e5257
Marcela Godoy <magodoycruz@gmail.com>
2025-05-21 07:44:38 -0300

    inclusão de modulos e funções

--- app.py
+++ app.py
@@ -1,13 +1,48 @@
-# Frescurinha de FSymbol para título bonitinho
-print('''
-█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
-▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
-''')
+import os
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurantes')
-print('3. Ativar Restaurante')
-print('4. Sair \n')
+def exibir_nome_restaurante():
+    # Frescurinha de FSymbol para título bonitinho
+    print('''
+    █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+    ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+    ''')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+def listar_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurantes')
+    print('3. Ativar Restaurante')
+    print('4. Sair \n')
+
+def escolher_opcoes():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    match opcao_escolhida:
+        case 1: 
+            cadastrar_restaurante()
+        case 2:
+            listar_restaurante()
+        case 3:
+            ativar_restaurante()
+        case _:
+            finalizar_app()
+
+def finalizar_app():
+    os.system('clear')
+    print('Finalizando o programa...\n')
+
+def cadastrar_restaurante():
+    print('Cadastrar restaurante')
+
+def listar_restaurante():
+    print('Listar restaurantes')
+
+def ativar_restaurante():
+    print('Ativar restaurante')
+
+def main():
+    exibir_nome_restaurante()
+    listar_opcoes()
+    escolher_opcoes()
+
+## Definindo como arquivo principal (main)
+if __name__ == '__main__':
+    main()
--- playground.py
+++ playground.py
@@ -1,12 +1,50 @@
-print('Python na Escola de Programação Alura \n')
+def play_01():
+    print('Python na Escola de Programação Alura \n')
 
-nome = 'Marcela'
-idade = 36
+    nome = 'Marcela'
+    idade = 36
 
-print(f'Meu nome é {nome} e eu tenho {idade} anos.\n')
+    print(f'Meu nome é {nome} e eu tenho {idade} anos.\n')
 
-print('a\nl\nu\nr\na\n')
+    print('a\nl\nu\nr\na\n')
 
-pi = 3.14159
-pi_arredondado = round(pi, 2) # Arredonda, exibindo 2 casas decimais (segundo parametro)
-print(f'O valor arredondado de pi é {pi_arredondado}')
+    pi = 3.14159
+    pi_arredondado = round(pi, 2) # Arredonda, exibindo 2 casas decimais (segundo parametro)
+    print(f'O valor arredondado de pi é {pi_arredondado}')
+
+def play_02():
+    # Determinar se o numero solicitado é par ou impar:
+    numero_escolhido = int(input('Digite um número:'))
+    resto_da_divisao_por_dois = numero_escolhido%2
+
+    if resto_da_divisao_por_dois == 0:
+        print('Este numero é PAR')
+    else: 
+        print('Este numero é IMPAR')
+
+def play_03():
+    # Classificar conforme idade do usuario se ele é uma criança, adolescente ou adulto
+    idade = int(input('Digite a sua idade:'))
+    if idade <=12:
+        print('Você é uma criança')
+    elif idade > 12 and idade <= 18:
+        print('Vocẽ é um adolescente')
+    else:
+        print('Você é um adulto')
+
+def play_04():
+    # a partir de coordenadas x e y dadas pelo usuário, determinar em qual quadrante do plano cartesiano o ponto se encontra
+    coordenada_x = int(input('Digite o valor da coordenada X:'))
+    coordenada_y = int(input('Agora digite o valor da coordenada Y:'))
+
+    if coordenada_x > 0 and coordenada_y > 0:
+        print('Coordenada está no primeiro quadrante')
+    elif coordenada_x < 0 and coordenada_y > 0:
+        print('Coordenada está no segundo quadrante')
+    elif coordenada_x < 0 and coordenada_y < 0:
+        print('Coordenada está no terceiro quadrante')
+    elif coordenada_x > 0 and coordenada_y < 0:
+        print('Coordenada está no quarto quadrante')
+    else:
+        print('Coordenada está no centro de origem')
+play_04()
